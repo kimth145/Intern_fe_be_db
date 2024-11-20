4dc2a16cee23a4f1f37af18e912cd543c5713876
ais081101 <kimth145@ais-info.co.jp>
2024-11-20 16:50:37 +0900

    feature : intern_fe_be_db

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const pg = require("pg");
+const app = express();
+
+const client = new pg.Pool({
+  //   user: "postgres",
+  //   password: "1234",
+  //   host: "localhost",
+  //   port: 5432,
+  //   database: "FastApi",
+  user: "intern_db_5lx4_user",
+  host: "dpg-csu2o6lumphs738kvg9g-a.oregon-postgres.render.com",
+  password: "jVWd305lnaZjRpdgBCN5dvD8EbwtZdz3",
+  port: 5432,
+  database: "intern_db_5lx4",
+  ssl: true,
+});
+
+app.use(cors());
+
+app.get("/menu", async (req, res) => {
+  await client.connect();
+  const result = await client.query(
+    "SELECT fruit_id, fruit_img FROM public.fruit_world"
+  );
+  res.send(result.rows);
+});
+
+app.listen(3001, () => {
+  console.log("connected on port 3001");
+});
